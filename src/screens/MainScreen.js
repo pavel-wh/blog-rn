10c058c259d06fbe2474d853c7983bd17958fe8a
pB <pavel.wh@yandex.ru>
2019-12-12 04:46:08 +0300

    Post display component

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,26 +1,30 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native'
+import { DATA } from '../data'
+import { Post } from '../components/Post'
 
 export const MainScreen = ({ navigation }) => {
-    const goToPost = () => {
-        navigation.navigate('Post')
-    }
-    return (
-        <View style={ styles.center }>
-            <Text>MainScreen</Text>
-            <Button title="Go to post" onPress={ goToPost } />
-        </View>
-    )
+  const goToPost = () => {
+    navigation.navigate('Post')
+  }
+
+  return (
+    <View style={styles.wrapper}>
+      <FlatList
+        data={DATA}
+        keyExtractor={post => post.id.toString()}
+        renderItem={({ item }) => <Post post={item} />}
+      />
+    </View>
+  )
 }
 
 MainScreen.navigationOptions = {
-    headerTitle: 'Мой блог'
+  headerTitle: 'Мой блог'
 }
 
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  wrapper: {
+    padding: 10
+  }
 })
