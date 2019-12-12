e737884168707884af3da1000cb01a82af7c91b9
pB <pavel.wh@yandex.ru>
2019-12-12 08:30:41 +0300

    Post screen styling

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,14 +1,43 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image, Button, ScrollView, Alert } from 'react-native'
 import { THEME } from '../theme'
+import { DATA } from '../data'
 
 export const PostScreen = ({ navigation }) => {
     const postId = navigation.getParam('postId')
+
+    const post = DATA.find(p => p.id === postId)
+
+    const removeHandler = () => {
+        Alert.alert(
+            'Удаление поста',
+            'Вы точно хотите удалить пост?',
+            [
+                {
+                    text: 'Отменить',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Удалить', 
+                    style: 'destructive',
+                    onPress: () => {}
+                }
+            ],
+            {
+                cancelable: false
+            },
+        )
+    }
+
     return (
-        <View style={ styles.center }>
-            <Text>PostScreen { postId }</Text>
-        </View>
-    )
+        <ScrollView>
+            <Image source={ { uri: post.img } } style={ styles.image } />
+            <View style={ styles.textWrap }>
+                <Text style={ styles.title }>{ post.text }</Text>
+            </View>
+            <Button title='Удалить' color={ THEME.DANGER_COLOR } onPress={ removeHandler } />
+        </ScrollView>
+    ) 
 }
 
 PostScreen.navigationOptions = ({ navigation }) => {
@@ -24,9 +53,14 @@ PostScreen.navigationOptions = ({ navigation }) => {
 }
 
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    textWrap: {
+        padding: 10
+    },
+    title: {
+        fontFamily: 'open-sans-regular'
     }
 }) 
