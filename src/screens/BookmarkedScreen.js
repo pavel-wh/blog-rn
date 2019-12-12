f8fe707a78f1c4d7e5e9d31777a896cf951d99ab
pB <pavel.wh@yandex.ru>
2019-12-12 10:20:52 +0300

    Bookmarked screen display

--- src/screens/BookmarkedScreen.js
+++ src/screens/BookmarkedScreen.js
@@ -1,18 +1,40 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet, FlatList } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { DATA } from '../data'
+import { Post } from '../components/Post'
+import { AppHeaderIcon } from '../components/AppHeaderIcon'
 
-export const BookmarkedScreen = ({}) => {
+export const BookmarkedScreen = ({ navigation }) => {
+    const openPostHandler = post => {
+        navigation.navigate('Post', {
+            postId: post.id,
+            date: post.date,
+            booked: post.booked
+        })
+    }
     return (
-        <View style={ styles.center }>
-            <Text>BookmarkedScreen</Text>
+        <View style={ styles.wrapper }>
+            <FlatList 
+                data={ DATA.filter(post => post.booked) } 
+                keyExtractor={ post => post.id.toString() }
+                renderItem={ ({ item }) => <Post post={ item } onOpen={ openPostHandler } /> }
+            />
         </View>
     )
 }
 
+BookmarkedScreen.navigationOptions = {
+    headerTitle: 'Избранное',
+    headerLeft: (
+        <HeaderButtons HeaderButtonComponent={ AppHeaderIcon }>
+            <Item title='menu' iconName='md-menu' onPress={ () => console.log('Press menu')}></Item>
+        </HeaderButtons>
+    )
+}
+
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    wrapper: {
+        padding: 10
     }
 })
